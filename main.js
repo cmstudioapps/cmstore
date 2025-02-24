c1d1c2cca526ca881f73bca0bf1e11b4f75e84ea
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-02-24 18:21:50 -0300

    Atualizar o main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.error(`Erro ao tentar entrar em tela cheia: ${err.message}`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+}
