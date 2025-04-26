521e3c37c0314419090b24d11950bf7e51566ad0
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-04-26 06:09:16 -0300

    Atualizar o ler.js

--- api/ler.js
+++ api/ler.js
@@ -1,15 +1,22 @@
 export default function handler (req, res) {
 
-const {sala} = req.query 
+const {sala,id} = req.query 
 const url = "https://palavras-22e2c-default-rtdb.firebaseio.com/"
+let p;
 fetch (url+sala+"/.json")
 .then(response => response.json())
 .then(data => {
+if(id === "s") {
+p = true
+} else {
+p = false
+}
 
  let msg = data.msg.toString()
  let nome = data.nome.toString()
 
- res.status(200).send(`${nome}: ${msg}`)
+
+ res.status(200).send(`${nome}:${p}: ${msg}`)
 
 }).catch (error => {
  res.status(400).send("ouve um erro ao tentar ler")
