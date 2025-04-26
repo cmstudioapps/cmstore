bdb2071f1263e133a1315ee159abc743e6c90873
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-04-26 02:14:27 -0300

    Atualizar o gett.js

--- api/gett.js
+++ api/gett.js
@@ -1,1 +1,22 @@
 /*Tratando requisição get como se fosse um envio de dados*/
+
+
+export default function handler(req, res) {
+
+const {acao, valor} = req.query
+
+if(acao === "salvar") {
+
+res.status(200).json({
+
+mensagem: `Olá ${valor}! Tudo bem?`
+
+})
+
+} else {
+ 
+res.status(400).json({erro: "Não conseguimos identificar"})
+
+}
+
+}
