b1a1f876c2e63e762562f6e357fffe12024c17f2
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-04-28 09:16:39 -0300

    Atualizar o mover.js

--- api/mover.js
+++ api/mover.js
@@ -2,6 +2,20 @@ export default function handler (req, res) {
 
 const url = "https://palavras-22e2c-default-rtdb.firebaseio.com/"
 
-const { x1, y1, x2, y2, sala } = req.query 
+const { x1, y1, x2, y2, sala, ver} = req.query 
+
+if(ver === "true") {
+fetch(url+sala+"/.json")
+.then(response => response.json())
+.then(data => {
+
+const X1 = data.x1
+const X2 = data.x2
+const Y1 = data.y1
+const Y2 = data.y2
+
+
+})
+}
 
 }
