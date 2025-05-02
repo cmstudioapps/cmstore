05d40abf22ea6252e84fcb0c5e419e2028569dbe
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-02 16:48:08 -0300

    Atualizar o hora.js

--- api/hora.js
+++ api/hora.js
@@ -1,6 +1,31 @@
 export default function handler (req, res) {
 
-const data = new Date()
+const { data } = req.query 
 
-res.status(200).send(data)
+const data = new Date()
+const dia = data.getDate()
+const mes = data.getMonth()
+const ano = data.getFullYear()
+
+
+if(data === "dia") {
+
+res.status(200).send(dia)
+
+
+}
+if(data === "mes") {
+
+res.status(200).send(mes)
+
+
+}
+if(data === "ano") {
+
+res.status(200).send(ano)
+
+
+}
+
+
 }
