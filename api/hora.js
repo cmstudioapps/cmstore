8005039645fa00a487e217add5d155ead9f933a5
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-02 16:53:31 -0300

    Atualizar o hora.js

--- api/hora.js
+++ api/hora.js
@@ -1,37 +1,25 @@
-export default function handler (req, res) {
+export default function handler(req, res) {
+  const { dataTime } = req.query;
 
-const { dataTime } = req.query 
+  const data = new Date();
+  const dia = data.getDate();
+  const mes = data.getMonth() + 1;
+  const ano = data.getFullYear();
 
-const data = new Date()
-const dia = data.getDate()
-const mes = data.getMonth()
-const ano = data.getFullYear()
+  if (dataTime === "dia") {
+    return res.status(200).send(dia.toString());
+  }
 
+  if (dataTime === "mes") {
+    return res.status(200).send(mes.toString());
+  }
 
-if(dataTime === "dia") {
+  if (dataTime === "ano") {
+    return res.status(200).send(ano.toString());
+  }
 
-res.status(200).send(dia)
-
-
-}
-if(dataTime === "mes") {
-
-res.status(200).send(mes)
-
-
-}
-if(dataTime === "ano") {
-
-res.status(200).send(ano)
-
-
-}
-
-if(!dataTime && !dataTime.includes("dia","mes","ano") {
-
-res.status(200).send("sem resposta")
-
-}
-
-
+  // Se não for nenhum dos casos acima:
+  if (!dataTime || !["dia", "mes", "ano"].includes(dataTime)) {
+    return res.status(200).send("sem resposta");
+  }
 }
