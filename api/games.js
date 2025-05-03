c6fe7192c49b4cab8ffa4c62d44b333c2531dfd3
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-03 09:12:18 -0300

    Atualizar o games.js

--- api/games.js
+++ api/games.js
@@ -2,12 +2,13 @@ export default function handler (req, res) {
 
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Access-Control-Allow-Methods', 'GET');
-
+/*
 const origin = req.headers.origin || req.headers.referer || "";
 
 if (!origin.includes("cm-store.vercel.app")) {
   return res.status(403).send("Acesso negado");
 }
+*/
 
 const url = "https://jogos-a1a46-default-rtdb.firebaseio.com/"
 const { games } = req.query 
