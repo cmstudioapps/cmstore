59dfa3a21342518629ef503d2ba31237d906051a
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-03 08:43:27 -0300

    Atualizar o games.js

--- api/games.js
+++ api/games.js
@@ -1,6 +1,6 @@
 export default function handler (req, res) {
 
-const url = ""
+const url = "https://jogos-a1a46-default-rtdb.firebaseio.com/"
 const { games } = req.query 
 
 if(games === "true") {
