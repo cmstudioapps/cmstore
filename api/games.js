9c67bfbbb4b879181d0127e57eff9cac06cdadd8
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-04 19:39:17 -0300

    Atualizar o games.js

--- api/games.js
+++ api/games.js
@@ -1,9 +1,11 @@
 export default function handler(req, res) {
   // Configurações de CORS seguras
   
-  res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', '*');
-res.setHeader('Access-Control-Allow-Headers', '*');
+  // Permite CORS apenas para o domínio especificado
+res.setHeader('Access-Control-Allow-Origin', 'https://cm-store.vercel.app');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.setHeader('Access-Control-Allow-Credentials', 'true'); // Opcional, se usar cookies/auth
 
   // URL do Firebase a partir de variáveis de ambiente
   const FIREBASE_URL = process.env.FIREBASE_URL || "https://jogos-a1a46-default-rtdb.firebaseio.com";
