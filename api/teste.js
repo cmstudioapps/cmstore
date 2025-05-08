c66090511de900e52e6d082a9848820a1f78a055
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-08 09:22:37 -0300

    Atualizar o teste.js

--- api/teste.js
+++ api/teste.js
@@ -2,6 +2,9 @@ export default function handler(req, res) {
 
 res.setHeader('Access-Control-Allow-Origin', '*');
 
-res.status(200).send("OPA iae")
+const { nome } = req.query 
+
+
+res.status(200).send("OPA iae "+nome)
 
 }
