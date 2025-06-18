c8d2a3d387eb553f8fb69e181bb7f1a7bbd69d79
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-06-18 19:03:45 -0300

    Atualizar o arquivo.js

--- api/arquivo.js
+++ api/arquivo.js
@@ -21,7 +21,7 @@ const arquivoURL = encodeURIComponent(nomeArquivo);
     }
 
     if (acao === "ler" && !valor) {
-        fetch(`${banco}/${jogoURL}/${arquivoURL}/${id}.json`)
+        fetch(`${banco}/${jogoURL}/${id}/${arquivoURL}.json`)
             .then(response => response.json())
             .then(data => {
                 if (data) {
@@ -36,7 +36,7 @@ const arquivoURL = encodeURIComponent(nomeArquivo);
     if (acao === "salvar") {
    const content = {valor}
    
-        fetch(`${banco}/${jogoURL}${id}/${arquivoURL}.json`, {
+        fetch(`${banco}/${jogoURL}/${id}/${arquivoURL}.json`, {
             method: "PATCH",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(content)
