b08608dc7b19847ae0545e5148dd3f63bdd836cd
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-06-18 15:17:30 -0300

    Atualizar o arquivo.js

--- api/arquivo.js
+++ api/arquivo.js
@@ -1,6 +1,18 @@
 export default function handler(req,res) {
 
+    // Permite CORS apenas para o domínio especificado
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+
+  
+  const banco = process.env.FIREBASE_URL || "https://jogos-a1a46-default-rtdb.firebaseio.com/Arquivos/";
+
+  // Pré-flight CORS
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
 
 }
